84f9a64d4c6f66c27981c0490d74058a518fe33b
Victor Galvez <vhgalvez@gmail.com>
2024-10-25 13:15:04 +0200

    Update main.tf

--- terraform/modules/ignition/main.tf
+++ terraform/modules/ignition/main.tf
@@ -1,11 +1,11 @@
 resource "libvirt_ignition" "bootstrap_ignition" {
   name    = "okd_bootstrap.ign"
   pool    = "default"
-  content = "../ignition_configs/bootstrap.ign"
+  content = "/home/victory/openshift-okd/terraform/ignition_configs/bootstrap.ign"
 }
 
 resource "libvirt_ignition" "master_ignition" {
   name    = "okd_master.ign"
   pool    = "default"
-  content = "../ignition_configs/master.ign"
+  content = "/home/victory/openshift-okd/terraform/ignition_configs/master.ign"
 }
