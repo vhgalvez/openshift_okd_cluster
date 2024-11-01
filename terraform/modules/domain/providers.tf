def209c2416b82af3777daaf15fff4a18bc33410
Victor Galvez <vhgalvez@gmail.com>
2024-11-01 23:25:24 +0100

    Update providers.tf

--- terraform/modules/domain/providers.tf
+++ terraform/modules/domain/providers.tf
@@ -1,22 +1,10 @@
 terraform {
-  required_version = "= 1.9.8"
+  required_version = "~>1.3"
 
   required_providers {
-    ct = {
-      source  = "poseidon/ct"
-      version = "~> 0.13.0"
-    }
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = ">= 0.8.0, < 0.9.0"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.2.0"
-    }
-    ignition = {
-      source  = "terraform-providers/ignition"
-      version = "~> 2.1.0"
+      version = "0.8.1"
     }
   }
 }
@@ -24,3 +12,4 @@ terraform {
 provider "libvirt" {
   uri = "qemu:///system"
 }
+
