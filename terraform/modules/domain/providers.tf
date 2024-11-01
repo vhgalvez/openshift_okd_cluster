bf4dbec0572483729493c99d99c398d083eca3d8
Victor Galvez <vhgalvez@gmail.com>
2024-11-01 23:02:51 +0100

    Update providers.tf

--- terraform/modules/domain/providers.tf
+++ terraform/modules/domain/providers.tf
@@ -2,9 +2,13 @@ terraform {
   required_version = "= 1.9.8"
 
   required_providers {
+    ignition = {
+      source  = "terraform-providers/ignition"
+      version = "~> 2.1"
+    }
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.8.0"
+      version = ">= 0.8.0, < 0.9.0"
     }
     template = {
       source  = "hashicorp/template"
