d7ce24c604ed65b758ffda404bb7620d445a97c1
Victor Galvez <vhgalvez@gmail.com>
2024-11-03 00:56:21 +0100

    update

--- terraform/modules/ignition/main.tf
+++ terraform/modules/ignition/main.tf
@@ -70,9 +70,9 @@ data "ignition_config" "startup" {
 }
 
 output "bootstrap_ignition" {
-  value = libvirt_volume.bootstrap_ignition
+  value = libvirt_volume.bootstrap_ignition.id
 }
 
 output "master_ignition" {
-  value = libvirt_volume.master_ignition
+  value = libvirt_volume.master_ignition.id
 }
