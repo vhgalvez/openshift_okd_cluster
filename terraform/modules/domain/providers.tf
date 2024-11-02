d511d79472deaca8e20e77da182d8d5a97fbe28d
Victor Galvez <vhgalvez@gmail.com>
2024-11-02 04:43:34 +0100

    update

--- terraform/modules/domain/providers.tf
+++ terraform/modules/domain/providers.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = "~>1.5"
+
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.8.1"
+      version = "~> 0.8.1"
     }
   }
 }
