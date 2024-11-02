9dc1e5f5fe486aff616cf1455aa19ba9cfc13859
Victor Galvez <vhgalvez@gmail.com>
2024-11-02 04:56:42 +0100

    Update main.tf

--- terraform/modules/ignition/main.tf
+++ terraform/modules/ignition/main.tf
@@ -1,11 +1,14 @@
+
+# Definición del recurso para el archivo Ignition del bootstrap
 resource "libvirt_ignition" "bootstrap_ignition" {
   name    = "okd_bootstrap.ign"
   pool    = "default"
-  content = file("${path.module}/../../ignition_configs/bootstrap.ign")
+  content = "/home/victory/openshift_okd_cluster/terraform/ignition_configs/bootstrap.ign"
 }
 
+# Definición del recurso para el archivo Ignition del master
 resource "libvirt_ignition" "master_ignition" {
   name    = "okd_master.ign"
   pool    = "default"
-  content = file("${path.module}/../../ignition_configs/master.ign")
+  content = "/home/victory/openshift_okd_cluster/terraform/ignition_configs/master.ign"
 }
