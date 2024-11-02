99cc12d032c916e957d7d4f09f87cdd0621824ae
Victor Galvez <vhgalvez@gmail.com>
2024-11-02 03:18:21 +0100

    Update providers.tf

--- terraform/modules/domain/providers.tf
+++ terraform/modules/domain/providers.tf
@@ -1,18 +1,18 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = "~>1.5"
 
   required_providers {
-    ct = {
-      source  = "poseidon/ct"
-      version = "~> 0.13.0"
-    }
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = ">= 0.8.1, < 0.9.0"
+      version = "0.8.1"
     }
     ignition = {
       source  = "community-terraform-providers/ignition"
       version = "2.1.0"
     }
   }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
 }
