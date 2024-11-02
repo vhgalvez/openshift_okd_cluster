81c5884de7f5ef0ca84657e823f74c60c99afedf
Victor Galvez <vhgalvez@gmail.com>
2024-11-02 04:38:14 +0100

    update

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,14 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "~> 0.7.1"
+    }
+  }
+}
+
 provider "libvirt" {
-  source  = "dmacvicar/libvirt"
-  version = "~> 0.7.1"
+  uri = "qemu:///system"
 }
 
 // ...existing code...
