d155c0b816694e7baad432c063c902f39062d72e
Victor Galvez <vhgalvez@gmail.com>
2024-11-02 15:15:21 +0100

    Update main.tf

--- terraform/modules/domain/main.tf
+++ terraform/modules/domain/main.tf
@@ -1,12 +1,48 @@
 # terraform\modules\domain\main.tf
 
+data "ignition_config" "startup" {
+  users = [
+    data.ignition_user.core.rendered,
+  ]
+
+  files = [
+    element(data.ignition_file.hostname.*.rendered, count.index),
+  ]
+
+  systemd = [
+    data.ignition_systemd_unit.mount-images.rendered,
+    data.ignition_systemd_unit.qemu-agent.rendered
+  ]
+  count = var.hosts
+}
+
+data "ignition_file" "hostname" {
+  path       = "/etc/hostname"
+  mode       = 420 # decimal 0644
+
+  content {
+    content = format(var.hostname_format, count.index + 1)
+  }
+
+  count = var.hosts
+}
+
+data "ignition_user" "core" {
+  name = "core"
+
+  # Example password: foobar
+  password_hash = "$5$XMoeOXG6$8WZoUCLhh8L/KYhsJN2pIRb3asZ2Xos3rJla.FA1TI7"
+  # Preferably use the ssh key auth instead
+  # ssh_authorized_keys = "${list()}"
+}
+
 resource "libvirt_domain" "okd_bootstrap" {
   name            = var.bootstrap.name
   description     = var.bootstrap.description
   vcpu            = var.bootstrap.vcpu
   memory          = var.bootstrap.memory * 1024 # MiB
   running         = true
-  coreos_ignition = var.bootstrap_ignition_id
+  coreos_ignition = data.ignition_config.startup[count.index].rendered
 
   disk {
     volume_id = var.bootstrap_volume_id
@@ -44,7 +80,7 @@ resource "libvirt_domain" "okd_controlplane_1" {
   vcpu            = var.controlplane_1.vcpu
   memory          = var.controlplane_1.memory * 1024 # MiB
   running         = true
-  coreos_ignition = var.master_ignition_id
+  coreos_ignition = data.ignition_config.startup[count.index].rendered
 
   disk {
     volume_id = var.controlplane_1_volume_id
@@ -81,7 +117,7 @@ resource "libvirt_domain" "okd_controlplane_2" {
   vcpu            = var.controlplane_2.vcpu
   memory          = var.controlplane_2.memory * 1024 # MiB
   running         = true
-  coreos_ignition = var.master_ignition_id
+  coreos_ignition = data.ignition_config.startup[count.index].rendered
 
   disk {
     volume_id = var.controlplane_2_volume_id
@@ -117,7 +153,7 @@ resource "libvirt_domain" "okd_controlplane_3" {
   vcpu            = var.controlplane_3.vcpu
   memory          = var.controlplane_3.memory * 1024 # MiB
   running         = true
-  coreos_ignition = var.master_ignition_id
+  coreos_ignition = data.ignition_config.startup[count.index].rendered
 
   disk {
     volume_id = var.controlplane_3_volume_id
