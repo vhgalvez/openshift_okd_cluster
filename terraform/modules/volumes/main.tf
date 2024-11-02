cecbcbe084d16708b7b45b37b1d980f20e917ad9
Victor Galvez <vhgalvez@gmail.com>
2024-11-02 23:22:11 +0100

    Update main.tf

--- terraform/modules/volumes/main.tf
+++ terraform/modules/volumes/main.tf
@@ -1,55 +1,211 @@
-terraform {
-  required_providers {
-    ignition = {
-      source  = "community-terraform-providers/ignition"
-      version = "2.1.0"
-    }
-    libvirt = {
-      source  = "dmacvicar/libvirt"
-      version = "0.8.1"
-    }
+# terraform\modules\domain\main.tf
+
+# Configuración de los archivos de Ignition para montar el directorio de imágenes Docker y el servicio del agente de QEMU
+
+data "ignition_systemd_unit" "mount_images" {
+  name    = "var-mnt-images.mount"
+  enabled = true
+  content = file("${path.module}/../../docker-images-mount/docker-images.mount")
+}
+
+data "ignition_systemd_unit" "qemu_agent" {
+  name    = "qemu-agent.service"
+  enabled = true
+  content = file("${path.module}/../../docker-images-mount/qemu-agent.service")
+}
+
+# Definición de la configuración de Ignition reutilizable
+module "ignition_config" {
+  source                  = "../ignition"
+  mount_images_content    = data.ignition_systemd_unit.mount_images.rendered
+  qemu_agent_content      = data.ignition_systemd_unit.qemu_agent.rendered
+  core_user_password_hash = "$6$hNh1nwO5OWWct4aZ$OoeAkQ4gKNBnGYK0ECi8saBMbUNeQRMICcOPYEu1bFuj9Axt4Rh6EnGba07xtIsGNt2wP9SsPlz543gfJww11/"
+}
+
+# Definición de las máquinas virtuales de OKD
+
+resource "libvirt_domain" "okd_bootstrap" {
+  name        = var.bootstrap.name
+  description = var.bootstrap.description
+  vcpu        = var.bootstrap.vcpu
+  memory      = var.bootstrap.memory * 1024 # MiB
+  running     = true
+  qemu_agent  = true
+
+  # Attach the Ignition volume as a disk
+  disk {
+    volume_id = libvirt_volume.okd_bootstrap.id
+    scsi      = false
+  }
+
+  # Use UEFI firmware without secure boot
+  firmware = "efi"
+
+  disk {
+    volume_id = var.bootstrap_volume_id
+    scsi      = false
+  }
+
+  cpu {
+    mode = "host-passthrough"
+  }
+
+  graphics {
+    type     = "vnc"
+    autoport = true
+  }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  network_interface {
+    network_id     = var.network_id
+    hostname       = var.bootstrap.name
+    addresses      = [var.bootstrap.address]
+    mac            = var.bootstrap.mac
+    wait_for_lease = true
   }
 }
 
-provider "ignition" {
-  // Configuration options
+# Definición de las máquinas de control plane
+
+resource "libvirt_domain" "okd_controlplane_1" {
+  name        = var.controlplane_1.name
+  description = var.controlplane_1.description
+  vcpu        = var.controlplane_1.vcpu
+  memory      = var.controlplane_1.memory * 1024 # MiB
+  running     = true
+  qemu_agent  = true
+
+  # Attach the Ignition volume as a disk
+  disk {
+    volume_id = libvirt_volume.okd_controlplane_1.id
+    scsi      = false
+  }
+
+  # Use UEFI firmware without secure boot
+  firmware = "efi"
+
+  disk {
+    volume_id = var.controlplane_1_volume_id
+    scsi      = false
+  }
+
+  cpu {
+    mode = "host-passthrough"
+  }
+
+  graphics {
+    type     = "vnc"
+    autoport = true
+  }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  network_interface {
+    network_id     = var.network_id
+    hostname       = var.controlplane_1.name
+    addresses      = [var.controlplane_1.address]
+    mac            = var.controlplane_1.mac
+    wait_for_lease = true
+  }
 }
 
-provider "libvirt" {
-  // Configuration options
+resource "libvirt_domain" "okd_controlplane_2" {
+  name        = var.controlplane_2.name
+  description = var.controlplane_2.description
+  vcpu        = var.controlplane_2.vcpu
+  memory      = var.controlplane_2.memory * 1024 # MiB
+  running     = true
+  qemu_agent  = true
+
+  # Attach the Ignition volume as a disk
+  disk {
+    volume_id = libvirt_volume.okd_controlplane_2.id
+    scsi      = false
+  }
+
+  # Use UEFI firmware without secure boot
+  firmware = "efi"
+
+  disk {
+    volume_id = var.controlplane_2_volume_id
+    scsi      = false
+  }
+
+  cpu {
+    mode = "host-passthrough"
+  }
+
+  graphics {
+    type     = "vnc"
+    autoport = true
+  }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  network_interface {
+    network_id     = var.network_id
+    hostname       = var.controlplane_2.name
+    addresses      = [var.controlplane_2.address]
+    mac            = var.controlplane_2.mac
+    wait_for_lease = true
+  }
 }
 
-# size = convert GiB to Bytes
-resource "libvirt_volume" "fedora_coreos" {
-  name   = "fedora_coreos.qcow2"
-  pool   = "default"
-  source = var.coreos_image
-}
+resource "libvirt_domain" "okd_controlplane_3" {
+  name        = var.controlplane_3.name
+  description = var.controlplane_3.description
+  vcpu        = var.controlplane_3.vcpu
+  memory      = var.controlplane_3.memory * 1024 # MiB
+  running     = true
+  qemu_agent  = true
 
-resource "libvirt_volume" "okd_bootstrap" {
-  name           = "okd_bootstrap.qcow2"
-  pool           = "default"
-  size           = var.bootstrap_volume_size * 1073741824
-  base_volume_id = libvirt_volume.fedora_coreos.id
-}
+  # Attach the Ignition volume as a disk
+  disk {
+    volume_id = libvirt_volume.okd_controlplane_3.id
+    scsi      = false
+  }
 
-resource "libvirt_volume" "okd_controlplane_1" {
-  name           = "okd_controlplane_1.qcow2"
-  pool           = "default"
-  size           = var.controlplane_1_volume_size * 1073741824
-  base_volume_id = libvirt_volume.fedora_coreos.id
-}
+  # Use UEFI firmware without secure boot
+  firmware = "efi"
 
-resource "libvirt_volume" "okd_controlplane_2" {
-  name           = "okd_controlplane_2.qcow2"
-  pool           = "default"
-  size           = var.controlplane_2_volume_size * 1073741824
-  base_volume_id = libvirt_volume.fedora_coreos.id
-}
+  disk {
+    volume_id = var.controlplane_3_volume_id
+    scsi      = false
+  }
 
-resource "libvirt_volume" "okd_controlplane_3" {
-  name           = "okd_controlplane_3.qcow2"
-  pool           = "default"
-  size           = var.controlplane_3_volume_size * 1073741824
-  base_volume_id = libvirt_volume.fedora_coreos.id
+  cpu {
+    mode = "host-passthrough"
+  }
+
+  graphics {
+    type     = "vnc"
+    autoport = true
+  }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  network_interface {
+    network_id     = var.network_id
+    hostname       = var.controlplane_3.name
+    addresses      = [var.controlplane_3.address]
+    mac            = var.controlplane_3.mac
+    wait_for_lease = true
+  }
 }
