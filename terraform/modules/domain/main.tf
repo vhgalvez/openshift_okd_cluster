24afd90e311c734a4104a32718e7952702b4dfd1
Victor Galvez <vhgalvez@gmail.com>
2024-11-02 15:48:30 +0100

    Update main.tf

--- terraform/modules/domain/main.tf
+++ terraform/modules/domain/main.tf
@@ -1,40 +1,48 @@
-# terraform\modules\domain\main.tf
+# Configuración de los archivos de Ignition para montar el directorio de imágenes Docker y el servicio del agente de QEMU
 
+data "ignition_systemd_unit" "mount_images" {
+  name    = "var-mnt-images.mount"
+  enabled = true
+  content = file("${path.module}/../../docker-images-mount/docker-images.mount")
+}
+
+data "ignition_systemd_unit" "qemu_agent" {
+  name    = "qemu-agent.service"
+  enabled = true
+  content = file("${path.module}/../../docker-images-mount/qemu-agent.service")
+}
+
+# Definición de la configuración de Ignition para cada host
 data "ignition_config" "startup" {
-  users = [
-    data.ignition_user.core.rendered,
-  ]
+  users = [data.ignition_user.core.rendered]
 
-  files = [
-    element(data.ignition_file.hostname.*.rendered, count.index),
-  ]
+  files = [for i in range(var.hosts) : data.ignition_file.hostname[i].rendered]
 
   systemd = [
-    data.ignition_systemd_unit.mount-images.rendered,
-    data.ignition_systemd_unit.qemu-agent.rendered
+    data.ignition_systemd_unit.mount_images.rendered,
+    data.ignition_systemd_unit.qemu_agent.rendered,
   ]
   count = var.hosts
 }
 
+# Configuración de archivos de hostname en cada host
 data "ignition_file" "hostname" {
+  count      = var.hosts
+  filesystem = "root"
   path       = "/etc/hostname"
-  mode       = 420 # decimal 0644
-
-  content {
-    content = format(var.hostname_format, count.index + 1)
-  }
-
-  count = var.hosts
+  mode       = 420
+  user       = "root"
+  group      = "root"
+  contents   = "${var.hostname_prefix}${count.index + 1}"
 }
 
+# Configuración del usuario core
 data "ignition_user" "core" {
-  name = "core"
-
-  # Example password: foobar
-  password_hash = "$5$XMoeOXG6$8WZoUCLhh8L/KYhsJN2pIRb3asZ2Xos3rJla.FA1TI7"
-  # Preferably use the ssh key auth instead
-  # ssh_authorized_keys = "${list()}"
+  name           = "core"
+  password_hash  = "$5$XMoeOXG6$8WZoUCLhh8L/KYhsJN2pIRb3asZ2Xos3rJla.FA1TI7"
 }
+
+# Definición de las máquinas virtuales de OKD
 
 resource "libvirt_domain" "okd_bootstrap" {
   name            = var.bootstrap.name
@@ -42,7 +50,7 @@ resource "libvirt_domain" "okd_bootstrap" {
   vcpu            = var.bootstrap.vcpu
   memory          = var.bootstrap.memory * 1024 # MiB
   running         = true
-  coreos_ignition = data.ignition_config.startup[count.index].rendered
+  coreos_ignition = data.ignition_config.startup[0].rendered
 
   disk {
     volume_id = var.bootstrap_volume_id
@@ -52,7 +60,6 @@ resource "libvirt_domain" "okd_bootstrap" {
   cpu {
     mode = "host-passthrough"
   }
-
 
   graphics {
     type     = "vnc"
@@ -74,13 +81,15 @@ resource "libvirt_domain" "okd_bootstrap" {
   }
 }
 
+# Definición de las máquinas de control plane
+
 resource "libvirt_domain" "okd_controlplane_1" {
   name            = var.controlplane_1.name
   description     = var.controlplane_1.description
   vcpu            = var.controlplane_1.vcpu
   memory          = var.controlplane_1.memory * 1024 # MiB
   running         = true
-  coreos_ignition = data.ignition_config.startup[count.index].rendered
+  coreos_ignition = data.ignition_config.startup[1].rendered
 
   disk {
     volume_id = var.controlplane_1_volume_id
@@ -90,7 +99,6 @@ resource "libvirt_domain" "okd_controlplane_1" {
   cpu {
     mode = "host-passthrough"
   }
-
 
   graphics {
     type     = "vnc"
@@ -111,13 +119,14 @@ resource "libvirt_domain" "okd_controlplane_1" {
     wait_for_lease = true
   }
 }
+
 resource "libvirt_domain" "okd_controlplane_2" {
   name            = var.controlplane_2.name
   description     = var.controlplane_2.description
   vcpu            = var.controlplane_2.vcpu
   memory          = var.controlplane_2.memory * 1024 # MiB
   running         = true
-  coreos_ignition = data.ignition_config.startup[count.index].rendered
+  coreos_ignition = data.ignition_config.startup[2].rendered
 
   disk {
     volume_id = var.controlplane_2_volume_id
@@ -147,13 +156,14 @@ resource "libvirt_domain" "okd_controlplane_2" {
     wait_for_lease = true
   }
 }
+
 resource "libvirt_domain" "okd_controlplane_3" {
   name            = var.controlplane_3.name
   description     = var.controlplane_3.description
   vcpu            = var.controlplane_3.vcpu
   memory          = var.controlplane_3.memory * 1024 # MiB
   running         = true
-  coreos_ignition = data.ignition_config.startup[count.index].rendered
+  coreos_ignition = data.ignition_config.startup[3].rendered
 
   disk {
     volume_id = var.controlplane_3_volume_id
@@ -175,8 +185,6 @@ resource "libvirt_domain" "okd_controlplane_3" {
     target_port = "0"
   }
 
-
-
   network_interface {
     network_id     = var.network_id
     hostname       = var.controlplane_3.name
@@ -184,17 +192,4 @@ resource "libvirt_domain" "okd_controlplane_3" {
     mac            = var.controlplane_3.mac
     wait_for_lease = true
   }
-}
-
-
-data "ignition_systemd_unit" "mount-images" {
-  name    = "var-mnt-images.mount"
-  enabled = true
-  content = file("${path.module}/../../docker-images-mount/docker-images.mount")
-}
-
-data "ignition_systemd_unit" "qemu-agent" {
-  name    = "qemu-agent.service"
-  enabled = true
-  content = file("${path.module}/../../docker-images-mount/qemu-agent.service")
 }
