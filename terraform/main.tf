90839ab002a49f06840652ffce761ecc92f9abd8
Victor Galvez <vhgalvez@gmail.com>
2024-11-02 01:59:13 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,9 +15,6 @@ module "volumes" {
   controlplane_3_volume_size = var.controlplane_3.volume_size
 }
 
-module "ignition" {
-  source = "./modules/ignition"
-}
 
 module "domain" {
   source     = "./modules/domain"
