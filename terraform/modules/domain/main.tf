0557e56ec961622814e0489d560e7e4566922d28
Victor Galvez <vhgalvez@gmail.com>
2024-11-02 19:40:15 +0100

    Update main.tf

--- terraform/modules/domain/main.tf
+++ terraform/modules/domain/main.tf
@@ -42,6 +42,20 @@ data "ignition_user" "core" {
   password_hash = "$6$hNh1nwO5OWWct4aZ$OoeAkQ4gKNBnGYK0ECi8saBMbUNeQRMICcOPYEu1bFuj9Axt4Rh6EnGba07xtIsGNt2wP9SsPlz543gfJww11/"
 }
 
+# Definición del recurso para el archivo Ignition del bootstrap
+resource "libvirt_ignition" "bootstrap_ignition" {
+  name    = "okd_bootstrap.ign"
+  pool    = "default"
+  content = file("/home/victory/openshift_okd_cluster/terraform/ignition_configs/bootstrap.ign")
+}
+
+# Definición del recurso para el archivo Ignition del master
+resource "libvirt_ignition" "master_ignition" {
+  name    = "okd_master.ign"
+  pool    = "default"
+  content = file("/home/victory/openshift_okd_cluster/terraform/ignition_configs/master.ign")
+}
+
 # Definición de las máquinas virtuales de OKD
 
 resource "libvirt_domain" "okd_bootstrap" {
@@ -53,7 +67,7 @@ resource "libvirt_domain" "okd_bootstrap" {
   qemu_agent  = true
 
   # Use the bootstrap ignition configuration
-  coreos_ignition = data.ignition_config.startup[0].rendered
+  coreos_ignition = libvirt_ignition.bootstrap_ignition.id
 
   disk {
     volume_id = var.bootstrap_volume_id
@@ -95,7 +109,7 @@ resource "libvirt_domain" "okd_controlplane_1" {
   qemu_agent  = true
 
   # Use the master ignition configuration
-  coreos_ignition = data.ignition_config.startup[0].rendered
+  coreos_ignition = libvirt_ignition.master_ignition.id
 
   disk {
     volume_id = var.controlplane_1_volume_id
@@ -135,7 +149,7 @@ resource "libvirt_domain" "okd_controlplane_2" {
   qemu_agent  = true
 
   # Use the master ignition configuration
-  coreos_ignition = data.ignition_config.startup[1].rendered
+  coreos_ignition = libvirt_ignition.master_ignition.id
 
   disk {
     volume_id = var.controlplane_2_volume_id
@@ -173,7 +187,7 @@ resource "libvirt_domain" "okd_controlplane_3" {
   memory          = var.controlplane_3.memory * 1024 # MiB
   running         = true
   qemu_agent      = true
-  coreos_ignition = data.ignition_config.startup[2].rendered
+  coreos_ignition = libvirt_ignition.master_ignition.id
 
   disk {
     volume_id = var.controlplane_3_volume_id
