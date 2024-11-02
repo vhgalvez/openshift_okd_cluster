ce3c60cd27fe97aef3c7f5b2f25a2ff94c9438e2
Victor Galvez <vhgalvez@gmail.com>
2024-11-02 02:03:30 +0100

    Update providers.tf

--- terraform/modules/domain/providers.tf
+++ terraform/modules/domain/providers.tf
@@ -10,9 +10,6 @@ terraform {
       source  = "dmacvicar/libvirt"
       version = ">= 0.8.0, < 0.9.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.2.0"
-    }
+
   }
 }
