42ff2b66cd70a94ecd28948baa116412a6d0f1fc
Victor Galvez <vhgalvez@gmail.com>
2024-11-02 20:47:52 +0100

    Update variables.tf

--- terraform/modules/domain/variables.tf
+++ terraform/modules/domain/variables.tf
@@ -1,73 +1,43 @@
 variable "network_id" {
-  type        = string
-  description = "value of network_id"
+  type = string
 }
 
 variable "bootstrap_volume_id" {
-  type        = string
-  description = "value of bootstrap_volume_id"
-}
-
-variable "bootstrap_ignition_id" {
-  type        = string
-  description = "value of bootstrap_ignition_id"
+  type = string
 }
 
 variable "controlplane_1_volume_id" {
-  type        = string
-  description = "value of controlplane_1_volume_id"
+  type = string
 }
 
 variable "controlplane_2_volume_id" {
-  type        = string
-  description = "value of controlplane_2_volume_id"
+  type = string
 }
 
 variable "controlplane_3_volume_id" {
-  type        = string
-  description = "value of controlplane_3_volume_id"
-}
-
-variable "master_ignition_id" {
-  type        = string
-  description = "value of master_ignition_id"
-}
-
-variable "hostname_prefix" {
-  type        = string
-  description = "Prefix for hostnames"
+  type = string
 }
 
 variable "bootstrap" {
-  type = map(string)
+  type = map(any)
 }
 
 variable "controlplane_1" {
-  type = map(string)
+  type = map(any)
 }
 
 variable "controlplane_2" {
-  type = map(string)
+  type = map(any)
 }
 
 variable "controlplane_3" {
-  type = map(string)
+  type = map(any)
 }
 
-variable "hosts" {
-  type        = number
-  description = "Number of hosts"
-  default     = 3
+variable "hostname_prefix" {
+  type = string
 }
 
 variable "controlplane_count" {
-  type        = number
-  description = "Number of control planes"
-  default     = 3
-}
-
-variable "proxy" {
-  type        = map(string)
-  description = "Proxy configuration for Ignition"
-  default     = {}
+  type = number
 }
