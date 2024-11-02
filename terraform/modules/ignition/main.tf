d18d3ffe34215a5dabf7428c03dd0837facbe6b5
Victor Galvez <vhgalvez@gmail.com>
2024-11-02 22:55:47 +0100

    update

--- terraform/modules/ignition/main.tf
+++ terraform/modules/ignition/main.tf
@@ -13,3 +13,40 @@ resource "libvirt_volume" "master_ignition" {
   source = "${path.module}/../../ignition_configs/master.ign"
   format = "raw"
 }
+
+data "ignition_systemd_unit" "mount_images" {
+  name    = "var-mnt-images.mount"
+  enabled = true
+  content = var.mount_images_content
+}
+
+data "ignition_systemd_unit" "qemu_agent" {
+  name    = "qemu-agent.service"
+  enabled = true
+  content = var.qemu_agent_content
+}
+
+data "ignition_user" "core" {
+  name          = "core"
+  password_hash = var.core_user_password_hash
+}
+
+data "ignition_file" "hostname" {
+  count = var.hosts
+  path  = "/etc/hostname"
+
+  content {
+    content = "${var.hostname_prefix}${count.index + 1}"
+  }
+}
+
+data "ignition_config" "startup" {
+  count = var.hosts
+
+  systemd = [
+    data.ignition_systemd_unit.mount_images.rendered,
+    data.ignition_systemd_unit.qemu_agent.rendered,
+  ]
+  users = [data.ignition_user.core.rendered]
+  files = [data.ignition_file.hostname[count.index].rendered]
+}
---
+++ terraform/modules/ignition/variables.tf
@@ -0,0 +1,19 @@
+variable "hosts" {
+  type = number
+}
+
+variable "hostname_prefix" {
+  type = string
+}
+
+variable "mount_images_content" {
+  type = string
+}
+
+variable "qemu_agent_content" {
+  type = string
+}
+
+variable "core_user_password_hash" {
+  type = string
+}
