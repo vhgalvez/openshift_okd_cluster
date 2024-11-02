270b7c9731eef6499ff9c49b26feb3e0630c8640
Victor Galvez <vhgalvez@gmail.com>
2024-11-03 00:43:12 +0100

    Update main.tf

--- terraform/modules/volumes/main.tf
+++ terraform/modules/volumes/main.tf
@@ -1,5 +1,18 @@
 # terraform\modules\volumes\main.tf
 
+terraform {
+  required_providers {
+    ignition = {
+      source  = "community-terraform-providers/ignition"
+      version = "2.1.0"
+    }
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.8.1"
+    }
+  }
+}
+
 resource "libvirt_volume" "fedora_coreos" {
   name   = "fedora_coreos.qcow2"
   pool   = "default"
