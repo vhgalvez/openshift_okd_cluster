038c4d75b06b3d67e4ea49ecbdbdaedb6bd7c783
Victor Galvez <vhgalvez@gmail.com>
2024-11-03 12:47:27 +0100

    Update main.tf

--- terraform/modules/domain/main.tf
+++ terraform/modules/domain/main.tf
@@ -303,4 +303,6 @@ output "bootstrap_ignition" {
   value = libvirt_volume.bootstrap_ignition.id
 }
 
-output "master_ignition
+output "master_ignition" {
+  value = libvirt_volume.master_ignition.id
+}
