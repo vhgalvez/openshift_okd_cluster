5e970162d7a87e463413f3a1cb168ddc780df313
Victor Galvez <vhgalvez@gmail.com>
2024-11-03 02:20:21 +0100

    Update variables.tf

--- terraform/modules/ignition/variables.tf
+++ terraform/modules/ignition/variables.tf
@@ -18,3 +18,11 @@ variable "qemu_agent_content" {
 variable "core_user_password_hash" {
   type = string
 }
+
+variable "bootstrap_ignition_id" {
+  type = string
+}
+
+variable "master_ignition_id" {
+  type = string
+}
