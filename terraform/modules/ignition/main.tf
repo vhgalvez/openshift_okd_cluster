ddd719fd58ca5297daa4bdcc80b766941519fe6e
Victor Galvez <vhgalvez@gmail.com>
2024-11-04 11:12:54 +0100

    Update main.tf

--- terraform/modules/ignition/main.tf
+++ terraform/modules/ignition/main.tf
@@ -21,7 +21,7 @@ provider "libvirt" {
 resource "libvirt_volume" "bootstrap_ignition" {
   name   = "bootstrap.ign"
   pool   = "default"
-  source = file("${path.module}/../../ignition_configs/bootstrap.ign")
+  source = file("${path.module}/../../ignition_configs/bootstrap.ign") // Ensure this path is correct
   format = "raw"
 }
 
@@ -29,7 +29,7 @@ resource "libvirt_volume" "bootstrap_ignition" {
 resource "libvirt_volume" "master_ignition" {
   name   = "master.ign"
   pool   = "default"
-  source = file("${path.module}/../../ignition_configs/master.ign")
+  source = file("${path.module}/../../ignition_configs/master.ign") // Ensure this path is correct
   format = "raw"
 }
 
