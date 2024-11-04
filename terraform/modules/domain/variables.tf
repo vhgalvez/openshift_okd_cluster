8a7e2a6d9d3b72893808a1febcb673ccb478df6b
Victor Galvez <vhgalvez@gmail.com>
2024-11-04 11:06:58 +0100

    Update variables.tf

--- terraform/modules/domain/variables.tf
+++ terraform/modules/domain/variables.tf
@@ -58,3 +58,11 @@ variable "master_ignition_id" {
 variable "core_user_password_hash" {
   type = string
 }
+
+variable "mount_images_content" {
+  type = string
+}
+
+variable "qemu_agent_content" {
+  type = string
+}
