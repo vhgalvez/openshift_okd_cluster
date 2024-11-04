90d6c3982062f89d64d33e8fa5a85362aa8bc110
Victor Galvez <vhgalvez@gmail.com>
2024-11-04 11:09:40 +0100

    update

--- terraform/modules/ignition/main.tf
+++ terraform/modules/ignition/main.tf
@@ -77,3 +77,11 @@ output "bootstrap_ignition" {
 output "master_ignition" {
   value = libvirt_volume.master_ignition.id
 }
+
+output "mount_images_content" {
+  value = data.ignition_systemd_unit.mount_images.rendered
+}
+
+output "qemu_agent_content" {
+  value = data.ignition_systemd_unit.qemu_agent.rendered
+}
