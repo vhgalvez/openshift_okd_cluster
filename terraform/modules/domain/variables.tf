586b675cdf087cc0c0889ca725d504e641e5c7d2
Victor Galvez <vhgalvez@gmail.com>
2024-11-05 14:59:51 +0100

    Update variables.tf

--- terraform/modules/domain/variables.tf
+++ terraform/modules/domain/variables.tf
@@ -1,9 +1,10 @@
 # terraform\modules\domain\variables.tf
-
+# Network ID for connecting the domains
 variable "network_id" {
   type = string
 }
 
+# Volume IDs for each domain
 variable "bootstrap_volume_id" {
   type = string
 }
@@ -20,6 +21,7 @@ variable "controlplane_3_volume_id" {
   type = string
 }
 
+# Domain configurations
 variable "bootstrap" {
   type = map(any)
 }
@@ -36,6 +38,7 @@ variable "controlplane_3" {
   type = map(any)
 }
 
+# Ignition configurations
 variable "bootstrap_ignition_id" {
   description = "ID del volumen Ignition para bootstrap"
   type        = string
