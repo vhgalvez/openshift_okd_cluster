de49037cf33f600bf0cf88f849f7d60872e1bd19
Victor Galvez <vhgalvez@gmail.com>
2024-11-05 13:57:21 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,17 +11,7 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-
-# Módulo para Ignition
-module "ignition" {
-  source                  = "./modules/ignition"
-  mount_images_content    = file("/home/victory/openshift_okd_cluster/terraform/qemu-agent/docker-images.mount")
-  qemu_agent_content      = file("/home/victory/openshift_okd_cluster/terraform/qemu-agent/qemu-agent.service")
-  core_user_password_hash = var.core_user_password_hash
-  hosts                   = var.controlplane_count + 1
-  hostname_prefix         = var.hostname_prefix
-}
-
+# Módulo de red
 module "network" {
   source         = "./modules/network"
   bootstrap      = var.bootstrap
@@ -29,18 +19,7 @@ module "network" {
   controlplane_2 = var.controlplane_2
   controlplane_3 = var.controlplane_3
 }
-
-# Modulo de red
-module "network" {
-  source         = "./modules/network"
-  bootstrap      = var.bootstrap
-  controlplane_1 = var.controlplane_1
-  controlplane_2 = var.controlplane_2
-  controlplane_3 = var.controlplane_3
-}
-
-
-# Modulo de volúmenes
+# Módulo de volúmenes
 module "volumes" {
   source                     = "./modules/volumes"
   coreos_image               = var.coreos_image
@@ -55,7 +34,7 @@ module "volumes" {
   controlplane_3             = var.controlplane_3
 }
 
-# Modulo de dominio
+# Módulo de dominio
 module "domain" {
   source                   = "./modules/domain"
   network_id               = module.network.okd_network_id
@@ -65,4 +44,14 @@ module "domain" {
   controlplane_3_volume_id = module.volumes.okd_controlplane_3_id
   bootstrap_ignition_id    = libvirt_volume.bootstrap_ignition.id
   master_ignition_id       = libvirt_volume.master_ignition.id
+}
+
+# Módulo de Ignition
+module "ignition" {
+  source                  = "./modules/ignition"
+  mount_images_content    = file("./qemu-agent/docker-images.mount")
+  qemu_agent_content      = file("./qemu-agent/qemu-agent.service")
+  core_user_password_hash = var.core_user_password_hash
+  hosts                   = var.controlplane_count + 1
+  hostname_prefix         = var.hostname_prefix
 }
