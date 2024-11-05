febc6f794df94b3d9b74efd3c1d3d4b13a47ec3a
Victor Galvez <vhgalvez@gmail.com>
2024-11-05 15:25:42 +0100

    Update variables.tf

--- terraform/modules/ignition/variables.tf
+++ terraform/modules/ignition/variables.tf
@@ -1,20 +1,71 @@
-# terraform\modules\ignition\variables.tf
-variable "hosts" {
-  type = number
+# modules/ignition/variables.tf
+
+variable "bootstrap" {
+  type = map(any)
+  description = "Configuration map for the bootstrap node"
 }
 
-variable "hostname_prefix" {
-  type = string
+variable "controlplane_1" {
+  type = map(any)
+  description = "Configuration map for control plane node 1"
 }
 
-variable "mount_images_content" {
-  type = string
+variable "controlplane_2" {
+  type = map(any)
+  description = "Configuration map for control plane node 2"
 }
 
-variable "qemu_agent_content" {
-  type = string
+variable "controlplane_3" {
+  type = map(any)
+  description = "Configuration map for control plane node 3"
+}
+
+variable "bootstrap_volume_id" {
+  type        = string
+  description = "Volume ID for the bootstrap node"
+}
+
+variable "controlplane_1_volume_id" {
+  type        = string
+  description = "Volume ID for control plane node 1"
+}
+
+variable "controlplane_2_volume_id" {
+  type        = string
+  description = "Volume ID for control plane node 2"
+}
+
+variable "controlplane_3_volume_id" {
+  type        = string
+  description = "Volume ID for control plane node 3"
+}
+
+variable "network_id" {
+  type        = string
+  description = "Network ID for all nodes"
 }
 
 variable "core_user_password_hash" {
-  type = string
+  type        = string
+  description = "Password hash for the core user"
+}
+
+variable "mount_images_content" {
+  type        = string
+  description = "Content for mounting Docker images"
+}
+
+variable "qemu_agent_content" {
+  type        = string
+  description = "Content for the QEMU agent service"
+}
+
+variable "hosts" {
+  type        = number
+  description = "Number of hosts"
+}
+
+variable "hostname_prefix" {
+  type        = string
+  description = "Prefix for hostnames"
 }
