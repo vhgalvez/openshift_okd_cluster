0d0da3ae5ca51bbd7bd99a19fbe2133fc0210fee
Victor Galvez <vhgalvez@gmail.com>
2024-11-05 04:32:20 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -91,6 +91,7 @@ variable "core_user_password_hash" {
   type    = string
   default = "$6$hNh1nwO5OWWct4aZ$OoeAkQ4gKNBnGYK0ECi8saBMbUNeQRMICcOPYEu1bFuj9Axt4Rh6EnGba07xtIsGNt2wP9SsPlz543gfJww11/"
 }
+
 # Variable para el ID de ignition del nodo bootstrap
 variable "bootstrap_ignition_id" {
   description = "ID del archivo Ignition para bootstrap"
