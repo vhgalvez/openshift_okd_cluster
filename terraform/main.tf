12a4c7e6203aae9789ab1783a9cca05f150e67e6
Victor Galvez <vhgalvez@gmail.com>
2024-11-05 05:14:16 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,6 +63,7 @@ module "network" {
 }
 
 # Configuración del módulo volumes
+
 module "volumes" {
   source                     = "./modules/volumes"
   coreos_image               = var.coreos_image
@@ -78,6 +79,34 @@ module "volumes" {
   controlplane_2             = var.controlplane_2
   controlplane_3             = var.controlplane_3
 }
+
+# Configuración del módulo domain
+module "domain" {
+  source     = "./modules/domain"
+  network_id = module.network.okd_network.id
+
+  mount_images_content = file("/home/victory/openshift_okd_cluster/terraform/qemu-agent/docker-images.mount")
+  qemu_agent_content   = file("/home/victory/openshift_okd_cluster/terraform/qemu-agent/qemu-agent.service")
+
+  # Referencias a volúmenes generados por el módulo `volumes`
+  bootstrap_volume_id      = module.volumes.okd_bootstrap_id
+  controlplane_1_volume_id = module.volumes.okd_controlplane_1_id
+  controlplane_2_volume_id = module.volumes.okd_controlplane_2_id
+  controlplane_3_volume_id = module.volumes.okd_controlplane_3_id
+
+  bootstrap                = var.bootstrap
+  controlplane_1           = var.controlplane_1
+  controlplane_2           = var.controlplane_2
+  controlplane_3           = var.controlplane_3
+  hostname_prefix          = var.hostname_prefix
+  controlplane_count       = var.controlplane_count
+  hosts                    = var.controlplane_count + 1
+  bootstrap_ignition_id    = libvirt_ignition.bootstrap_ignition.id
+  master_ignition_id       = libvirt_ignition.master_ignition.id
+  core_user_password_hash  = var.core_user_password_hash
+}
+
+
 
 # Configuración del módulo domain
 module "domain" {
