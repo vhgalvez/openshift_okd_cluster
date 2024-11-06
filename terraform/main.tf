db93c716145adec8cf68662fd152c1f4088b1073
Victor Galvez <vhgalvez@gmail.com>
2024-11-06 18:46:56 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,11 +26,6 @@ module "network" {
 module "volumes" {
   source                     = "./modules/volumes"
   coreos_image               = var.coreos_image
-  bootstrap_volume_size      = var.bootstrap.volume_size
-  controlplane_1_volume_size = var.controlplane_1.volume_size
-  controlplane_2_volume_size = var.controlplane_2.volume_size
-  controlplane_3_volume_size = var.controlplane_3.volume_size
-  network_id                 = module.network.okd_network_id
   bootstrap                  = var.bootstrap
   controlplane_1             = var.controlplane_1
   controlplane_2             = var.controlplane_2
@@ -40,44 +35,22 @@ module "volumes" {
 # Módulo de Configuración Ignition
 module "ignition" {
   source                   = "./modules/ignition"
-  bootstrap                = var.bootstrap
-  controlplane_1           = var.controlplane_1
-  controlplane_2           = var.controlplane_2
-  controlplane_3           = var.controlplane_3
-  bootstrap_volume_id      = module.volumes.okd_bootstrap_id
-  controlplane_1_volume_id = module.volumes.okd_controlplane_1_id
-  controlplane_2_volume_id = module.volumes.okd_controlplane_2_id
-  controlplane_3_volume_id = module.volumes.okd_controlplane_3_id
-  network_id               = module.network.okd_network_id
-  core_user_password_hash  = var.core_user_password_hash
-  hosts                    = var.controlplane_count + 1
-  hostname_prefix          = var.hostname_prefix
-  mount_images_content     = file("./qemu-agent/docker-images.mount")
-  qemu_agent_content       = file("./qemu-agent/qemu-agent.service")
 }
 
 # Módulo para Dominios (Máquinas Virtuales)
 module "domain" {
   source                   = "./modules/domain"
-  network_id               = module.network.okd_network_id
-  bootstrap_volume_id      = module.volumes.okd_bootstrap_id
-  controlplane_1_volume_id = module.volumes.okd_controlplane_1_id
-  controlplane_2_volume_id = module.volumes.okd_controlplane_2_id
-  controlplane_3_volume_id = module.volumes.okd_controlplane_3_id
-
-  # IDs de los volúmenes desde el módulo de volúmenes
-  okd_bootstrap_id         = module.volumes.okd_bootstrap_id
-  okd_controlplane_1_id    = module.volumes.okd_controlplane_1_id
-  okd_controlplane_2_id    = module.volumes.okd_controlplane_2_id
-  okd_controlplane_3_id    = module.volumes.okd_controlplane_3_id
-
-  # Contenido Ignition desde el módulo de ignition
-  bootstrap_ignition = module.ignition.bootstrap_ignition_content
-  master_ignition    = module.ignition.master_ignition_content
-
+  volumes = {
+    bootstrap = module.volumes.bootstrap.id
+    controlplane_1 = module.volumes.controlplane_1.id
+    controlplane_2 = module.volumes.controlplane_2.id
+    controlplane_3 = module.volumes.controlplane_3.id
+  }
   bootstrap      = var.bootstrap
   controlplane_1 = var.controlplane_1
   controlplane_2 = var.controlplane_2
   controlplane_3 = var.controlplane_3
+  bootstrap_ignition = module.ignition.bootstrap_ignition_content
+  master_ignition    = module.ignition.master_ignition_content
 }
 
