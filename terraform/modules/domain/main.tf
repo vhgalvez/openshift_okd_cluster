765e0fd2811a78ab8b493930015976af75898b7e
Victor Galvez <vhgalvez@gmail.com>
2024-11-06 19:30:40 +0100

    Update main.tf

--- terraform/modules/domain/main.tf
+++ terraform/modules/domain/main.tf
@@ -12,7 +12,7 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-# Recurso para el nodo bootstrap
+// Recurso para el nodo bootstrap
 resource "libvirt_domain" "okd_bootstrap" {
   name       = var.bootstrap["name"]
   memory     = var.bootstrap["memory"] * 1024
@@ -43,7 +43,7 @@ resource "libvirt_domain" "okd_bootstrap" {
   }
 }
 
-# Recurso para el nodo de control plane 1
+// Recurso para el nodo de control plane 1
 resource "libvirt_domain" "okd_controlplane_1" {
   name       = var.controlplane_1["name"]
   memory     = var.controlplane_1["memory"] * 1024
@@ -74,7 +74,7 @@ resource "libvirt_domain" "okd_controlplane_1" {
   }
 }
 
-# Recurso para el nodo de control plane 2
+// Recurso para el nodo de control plane 2
 resource "libvirt_domain" "okd_controlplane_2" {
   name       = var.controlplane_2["name"]
   memory     = var.controlplane_2["memory"] * 1024
@@ -105,7 +105,7 @@ resource "libvirt_domain" "okd_controlplane_2" {
   }
 }
 
-# Recurso para el nodo de control plane 3
+// Recurso para el nodo de control plane 3
 resource "libvirt_domain" "okd_controlplane_3" {
   name       = var.controlplane_3["name"]
   memory     = var.controlplane_3["memory"] * 1024
